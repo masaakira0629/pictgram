8cfd280d1a0bf1135f56fbc3fa433c516e5587dc
masaakira0629 <masaakira0629@yahoo.co.jp>
2019-01-09 14:44:26 +0900

    second commit

--- Gemfile
+++ Gemfile
@@ -35,6 +35,10 @@ gem 'jbuilder', '~> 2.5'
 
   gem 'bootstrap-sass'
 
+  gem "jquery-rails"
+
+  gem 'bcrypt'
+
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.1.0', require: false
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ApplicationRecord
   validates :name, presence: true
+  validates :name, length: { maximum: 15}
   validates :email, presence: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :password, presence: true
+  validates :password, length:{ in: 8..32}
+  VALID_PW_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  has_secure_password
 end
