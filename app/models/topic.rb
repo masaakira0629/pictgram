78a282a3ea9aeb198b17e42c860a8f259704f3be
masaakira0629 <masaakira0629@yahoo.co.jp>
2019-01-14 20:02:43 +0900

    forth commit

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -3,4 +3,11 @@ class Topic < ApplicationRecord
   validates :description, presence: true
   validates :image, presence: true
 
+  def extension_white_list
+      ['jpg', 'jpeg', 'png']
+  end
+
+  belongs_to :user
+
+  mount_uploader :image, ImageUploader
 end
--- test/controllers/topics_controller_test.rb
+++ test/controllers/topics_controller_test.rb
@@ -1,8 +1,13 @@
 require 'test_helper'
 
 class TopicsControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get topics_new_url
+  test "should get index" do
+    get topics_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get topics_show_url
     assert_response :success
   end
 
