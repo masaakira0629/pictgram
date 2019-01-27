e21cf82038b7b3d97e54c34e835d096e5dede958
masaakira0629 <masaakira0629@yahoo.co.jp>
2019-01-27 17:16:28 +0900

    first commit

--- app/models/like.rb
+++
@@ -1,5 +0,0 @@
-class Like < ApplicationRecord:Base
-
-  belongs_to :tweet, counter_cache: :likes_count
-  belongs_to :user
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_25_065021) do
+ActiveRecord::Schema.define(version: 2019_01_27_081237) do
 
   create_table "favorites", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id"
     t.string "topic_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "likes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "topic_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
